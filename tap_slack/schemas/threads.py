d38e19aef30c10c903623b9932e8edb1fb175e17
Minor Gordon <Minor.Gordon@onna.com>
2022-05-04 15:45:19 -0400

    disable 'files'

--- tap_slack/schemas/threads.py
+++ tap_slack/schemas/threads.py
@@ -26,13 +26,12 @@ schema = th.PropertiesList(
             th.Property("user", th.StringType, required=True),
         ),
     ),
-    th.Property("files", th.StringType),
+    # th.Property("files", th.StringType),
     th.Property("upload", th.StringType),
     th.Property("parent_user_id", th.StringType),
     th.Property("display_as_bot", th.StringType),
     th.Property("upload", th.StringType),
     th.Property("is_locked", th.BooleanType),
-    th.Property("files", th.StringType),
     th.Property(
         "blocks",
         th.ArrayType(
