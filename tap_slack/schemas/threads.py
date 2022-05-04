d826fea29660fa0b251bc910e14f5270bb169407
Minor Gordon <Minor.Gordon@onna.com>
2022-05-04 15:53:00 -0400

    bring threads schema into line with singer.io tap-slack

--- tap_slack/schemas/threads.py
+++ tap_slack/schemas/threads.py
@@ -1,7 +1,33 @@
 from singer_sdk import typing as th
 
 schema = th.PropertiesList(
+    th.Property(
+        "blocks",
+        th.ArrayType(
+            th.ObjectType(
+                th.Property("type", th.StringType),
+            )
+        ),
+    ),
     th.Property("channel_id", th.StringType, required=True),
+    th.Property("client_msg_id", th.StringType),
+    # th.Property("display_as_bot", th.StringType),
+    th.Property(
+        "edited",
+        th.ObjectType(
+            th.Property("ts", th.StringType, required=True),
+            th.Property("user", th.StringType, required=True),
+        ),
+    ),
+    # th.Property("files", th.StringType),
+    # th.Property("is_locked", th.BooleanType),
+    th.Property("latest_reply", th.StringType),
+    th.Property("reply_count", th.IntegerType),
+    th.Property("reply_users", th.ArrayType(th.StringType)),
+    th.Property("reply_users_count", th.NumberType),
+    th.Property("subscribed", th.BooleanType),
+    th.Property("team", th.StringType),
+    th.Property("text", th.StringType),
     th.Property(
         "thread_ts",
         th.StringType,
@@ -14,35 +40,7 @@ schema = th.PropertiesList(
         required=True,
         description="Epoch timestamp of when the thread reply was posted.",
     ),
-    th.Property("client_msg_id", th.StringType),
     th.Property("type", th.StringType),
-    th.Property("text", th.StringType),
+    # th.Property("upload", th.BooleanType),
     th.Property("user", th.StringType),
-    th.Property("team", th.StringType),
-    th.Property(
-        "edited",
-        th.ObjectType(
-            th.Property("ts", th.StringType, required=True),
-            th.Property("user", th.StringType, required=True),
-        ),
-    ),
-    # th.Property("files", th.StringType),
-    th.Property("upload", th.StringType),
-    th.Property("parent_user_id", th.StringType),
-    th.Property("display_as_bot", th.StringType),
-    th.Property("upload", th.StringType),
-    th.Property("is_locked", th.BooleanType),
-    th.Property(
-        "blocks",
-        th.ArrayType(
-            th.ObjectType(
-                th.Property("type", th.StringType),
-            )
-        ),
-    ),
-    th.Property("reply_count", th.IntegerType),
-    th.Property("reply_users_count", th.NumberType),
-    th.Property("latest_reply", th.StringType),
-    th.Property("reply_users", th.ArrayType(th.StringType)),
-    th.Property("subscribed", th.BooleanType),
 ).to_dict()
